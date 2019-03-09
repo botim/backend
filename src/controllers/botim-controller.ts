c67b7de9d34e91f72774dee8ebcd241f7f298258
eranshmil <eranshmil@users.noreply.github.com>
2019-03-09 23:39:38 +0200

    refactor: change confirmed route to match the ui

--- src/controllers/botim-controller.ts
+++ src/controllers/botim-controller.ts
@@ -1,29 +1,13 @@
-import * as express from 'express';
+import * as NodeCache from 'node-cache';
+import { Body, Controller, Query, Get, Post, Response, Route, Security, Tags } from 'tsoa';
+
 import { getBotsByIds } from '../data/confirmed';
 import { createNewReport } from '../data/suspected';
-import * as NodeCache from 'node-cache';
-import {
-  Body,
-  Controller,
-  Query,
-  Delete,
-  Get,
-  Header,
-  Path,
-  Post,
-  Put,
-  Request,
-  Response,
-  Route,
-  Security,
-  SuccessResponse,
-  Tags
-} from 'tsoa';
-import { Bots, Report, Bot, Platform } from '../models/symbols';
+import { BotMap, Report, Platform } from '../models/symbols';
 
 const botsCache = new NodeCache({
-  stdTTL: parseInt(process.env.CACHE_TTL || '3600'),
-  checkperiod: parseInt(process.env.CACHE_CHECK_PERIOD || '1800')
+  stdTTL: +process.env.CACHE_TTL || 3600,
+  checkperiod: +process.env.CACHE_CHECK_PERIOD || 1800
 });
 
 @Tags('Bots')
@@ -37,47 +21,46 @@ export class BotimController extends Controller {
   public async getConfirmed(
     @Query() userIds: string[],
     @Query() platform: Platform
-  ): Promise<Bots> {
-    /** Try to retrieve bots from the cache before reading data from the DB. */
+  ): Promise<BotMap> {
+    // /** Try to retrieve bots from the cache before reading data from the DB. */
+    // const cachedBots: Bots = {};
+    // let failToRetriveFromCache = false;
+    // /** Iterate on userIds to look for cached data about him. */
+    // for (const userId of userIds) {
+    //   const cachedBot: Bot | string = botsCache.get(`${platform}:${userId}`);
+    //   /** If not *all* users cached, abort and retrieve all from db. */
+    //   if (!cachedBot) {
+    //     failToRetriveFromCache = true;
+    //     break;
+    //   }
 
-    const cachedBots: Bots = {};
-    let failToRetriveFromCache = false;
-    /** Iterate on userIds to look for cached data about him. */
-    for (const userId of userIds) {
-      const cachedBot: Bot | string = botsCache.get(`${platform}:${userId}`);
-      /** If not *all* users cached, abort and retrieve all from db. */
-      if (!cachedBot) {
-        failToRetriveFromCache = true;
-        break;
-      }
+    //   /**
+    //    * If the user marked as 'NOT_EXIST' ignore it.
+    //    */
+    //   if (cachedBot !== 'NOT_EXIST') {
+    //     cachedBots[userId] = cachedBot as Bot;
+    //   }
+    // }
 
-      /**
-       * If the user marked as 'NOT_EXIST' ignore it.
-       */
-      if (cachedBot !== 'NOT_EXIST') {
-        cachedBots[userId] = cachedBot as Bot;
-      }
-    }
-
-    /** If all users cached, return the bots from the cache. */
-    if (!failToRetriveFromCache) {
-      return cachedBots;
-    }
+    // /** If all users cached, return the bots from the cache. */
+    // if (!failToRetriveFromCache) {
+    //   return cachedBots;
+    // }
 
     /** If cache not hold all bots yet, update cache for next time ;) */
     const bots = await getBotsByIds(userIds, platform);
 
-    /** Update cache. */
-    for (const [userId, confirmedBot] of Object.entries(bots)) {
-      botsCache.set(`${platform}:${userId}`, confirmedBot);
-    }
+    // /** Update cache. */
+    // for (const [userId, confirmedBot] of Object.entries(bots)) {
+    //   botsCache.set(`${platform}:${userId}`, confirmedBot);
+    // }
 
-    /** Mark all users that not in bots as 'NOT_EXIST' in the cache for next time. */
-    for (const userId of userIds) {
-      if (!(userId in bots)) {
-        botsCache.set(`${platform}:${userId}`, 'NOT_EXIST');
-      }
-    }
+    // /** Mark all users that not in bots as 'NOT_EXIST' in the cache for next time. */
+    // for (const userId of userIds) {
+    //   if (!(userId in bots)) {
+    //     botsCache.set(`${platform}:${userId}`, 'NOT_EXIST');
+    //   }
+    // }
 
     return bots;
   }
--- src/data/confirmed.ts
+++ src/data/confirmed.ts
@@ -1,22 +1,22 @@
 import { db } from '../core/db';
-import { Bots, Report, DetectionStatus, Platform } from '../models/symbols';
+import { BotMap, Platform } from '../models/symbols';
 
-export const getBotsByIds = async (userIds: string[], platform: Platform): Promise<Bots> => {
+export const getBotsByIds = async (userIds: string[], platform: Platform): Promise<BotMap> => {
   /** Gets all bots that mention in userIds array */
-  const botsArray = await db.any(
+  const bots = await db.any(
     `SELECT * FROM botim WHERE user_id IN ($1:list) AND platform = $2`,
     [userIds, platform]
   );
 
-  /** Convert array to bots object mapped by userId */
-  const botsMap: Bots = {};
-  for (const bot of botsArray) {
-    botsMap[bot.user_id] = {
-      botReason: bot.bot_reason,
-      detectionStatus: bot.detection_status,
-      platform: bot.platform
-    };
+  const botMap: BotMap = {};
+
+  for (const userId of userIds) {
+    botMap[userId] = 'NOT_BOT';
   }
 
-  return botsMap;
+  for (const bot of bots) {
+    botMap[bot.user_id] = bot.detection_status;
+  }
+
+  return botMap;
 };
