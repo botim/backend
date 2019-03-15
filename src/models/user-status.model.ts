c7cc98c8d9a8f26a77ce2be7efc4c9b53d07cb3e
eranshmil <eranshmil@users.noreply.github.com>
2019-03-15 12:15:58 +0200

    fix: typo and minor ocd changes

--- src/models/user-status.model.ts
+++ src/models/user-status.model.ts
@@ -5,26 +5,15 @@ import { AuthenticatedRequest } from './authenticated-request.model';
 import { Platform, Status, Reason } from '../core';
 
 @Entity({ name: 'user_statuses' })
-@Unique(['userId', 'platform', 'postId', 'commentId', 'replayCommentId'])
+@Unique(['platform', 'userId', 'postId', 'commentId', 'replyCommentId'])
 export class UserStatus extends AuthenticatedRequest {
   @PrimaryGeneratedColumn() private id: number;
-
-  @Column({ name: 'user_id', type: 'varchar', length: 30, nullable: false })
-  public userId: string;
-
-  @Column({ type: 'varchar', length: 200, nullable: true })
-  public description: string;
 
   @Column({ type: 'enum', enum: Platform, nullable: false })
   public platform: Platform;
 
-  // TODO: default not working when accessing the enum
-  // TypeError: Cannot read property 'REPORTED' of undefined
-  @Column({ type: 'enum', enum: Status, nullable: false })
-  public status?: Status;
-
-  @Column({ type: 'enum', enum: Reason, array: true, nullable: false })
-  public reasons: Reason[];
+  @Column({ name: 'user_id', type: 'varchar', length: 30, nullable: false })
+  public userId: string;
 
   @Column({ name: 'post_id', type: 'varchar', length: 30, nullable: true })
   public postId?: string;
@@ -32,8 +21,19 @@ export class UserStatus extends AuthenticatedRequest {
   @Column({ name: 'comment_id', type: 'varchar', length: 30, nullable: true })
   public commentId?: string;
 
-  @Column({ name: 'replay_comment_id', type: 'varchar', length: 30, nullable: true })
-  public replayCommentId?: string;
+  @Column({ name: 'reply_comment_id', type: 'varchar', length: 30, nullable: true })
+  public replyCommentId?: string;
+
+  @Column({ type: 'enum', enum: Reason, array: true, nullable: false })
+  public reasons: Reason[];
+
+  // TODO: default not working when accessing the enum
+  // TypeError: Cannot read property 'REPORTED' of undefined
+  @Column({ type: 'enum', enum: Status, nullable: false })
+  public status?: Status;
+
+  @Column({ type: 'varchar', length: 200, nullable: true })
+  public description: string;
 
   constructor(private userStatus?: Partial<UserStatus>) {
     super(userStatus);
