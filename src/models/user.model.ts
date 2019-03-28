543b38961356b34b7cd66e04ba9bf8af56c227b4
eranshmil <eranshmil@users.noreply.github.com>
2019-03-28 22:34:04 +0200

    feat: encrypt passwords

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class User {
@@ -7,7 +9,6 @@ export class User {
   @Column({ type: 'varchar', length: 30, nullable: false })
   public username: string;
 
-  /** Passwords should be encrypted using 'bcrypt' */
   @Column({ type: 'varchar', length: 128, nullable: false })
   public password: string;
 
@@ -18,5 +19,10 @@ export class User {
     if (user) {
       Object.assign(this, user);
     }
+  }
+
+  @BeforeInsert()
+  beforeInsert() {
+    this.password = bcrypt.hashSync(this.password, 10);
   }
 }
